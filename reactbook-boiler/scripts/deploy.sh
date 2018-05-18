26639bbf50d24feb0fe7948b8c328dd46aa629f4
noah398 <869tjmw@gmail.com>
2018-05-18 13:57:58 +0900

    5.4 デプロイ

--- reactbook-boiler/scripts/deploy.sh
+++ reactbook-boiler/scripts/deploy.sh
@@ -6,9 +6,9 @@ mkdir __deployme
 sh scripts/build.sh
 
 # minify js
-uglify -s bundle.js -o __deployme/bundle.js
+./node_modules/.bin/uglify -s bundle.js -o __deployme/bundle.js
 # minify css
-cssshrink bundle.css > __deployme/bundle.css
+./node_modules/.bin/cssshrink bundle.css > __deployme/bundle.css
 # copy html and images
 cp index.html __deployme/index.html
 cp -r images/ __deployme/images/
